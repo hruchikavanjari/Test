8196c7deb99c21e3ac5ef8c82195e63cd5c561f4
hruchikavanjari <hruchivanjari0221@gmail.com>
2022-02-09 16:35:56 +0530

    update index file

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
-<?php echo '<p>Hello hruchika vanjari motoaaaaaaaaaaa</p>'; ?> 
+<?php echo '<p>Hello hruchika vanjari 1111111111?> 
  </body>
 </html>
