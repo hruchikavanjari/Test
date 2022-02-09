d80d0ae6d794338a2809bdf9961e9bf6486dc823
Hruchika vanjari <97451538+hruchikavanjari@users.noreply.github.com>
2022-02-09 19:03:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
 <?php echo '<p>Hello hruchika vanjari motoaaddddddddddddaaaaaaaaa</p>'; ?> 
-<?php echo '<p>hhhhhhhhhhhhhhhhhhhhhh</p>'; ?> 
+<?php echo '<p>d</p>'; ?> 
  </body>
 </html>
