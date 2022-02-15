bc405f73c37413d979530ad08a259e46d86b3cf2
hruchikavanjari <hruchivanjari0221@gmail.com>
2022-02-15 10:20:57 +0530

    update index

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
-<?php echo '<p>Hello hruchika kamalakar vanjari </p>'; ?> 
- </body>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+<div class="container color">
+<h3>hi</h3>
+</div>
+<?php echo '<p>Hello World</p>'; ?> 
+<?php echo '<p>Hello hruchika vanjari </p>'; ?> 
+</body>
 </html>
